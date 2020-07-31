d41cef5679058d448f89bc5731d92489155b05c6
alexandrmotologa <al.motologa@gmail.com>
2020-07-31 17:01:21 +0300

    cam asa

--- hw_wrappers.py
+++ hw_wrappers.py
@@ -1,4 +1,11 @@
-def wrap_brackets( text ):
-  return "<"*3 + "["*3 + "(" + text + ")" + "]"*3 + ">"*3
 
-print( wrap_brackets("12345") )
+def wrap_brackets( text ):
+  return "(" + text + ")"
+
+def wrap_brackets_sq( text ):
+  return "["*3 + text + "]"*3
+
+def wrap_brackets_tr( text ):
+  return "<"*3 + text + ">"*3
+
+print(wrap_brackets_tr(wrap_brackets_sq(wrap_brackets("12345"))))
